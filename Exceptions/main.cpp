6249f31c8960609f58706b94118a04e6c4f4d491
PC <david.iii.chiu@gmail.com>
2022-03-03 21:33:17 +0800

    none

--- Exceptions/main.cpp
+++ Exceptions/main.cpp
@@ -1,6 +1,27 @@
+//  try: Allows you to define a block of code to be tested for errors while it's being executed.
+//  throw: Throws an exception when a problem is detected, which lets us create a custom error.
+//  catch: Allows you to define a block of code to be executed, if an error occurs in the try block.
+
+//  try & catch keywords come is pairs.
+
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    try {
+        int age;
+        cin >> age;
+        if (age >= 18) {
+            cout << "Access granted - You're old enough.";
+        } else {
+            throw (age);
+        }
+    } catch (int num) {
+        cout << "Access denied - You must be an adult to watch porn.\n";
+        cout << "Your age: " << num;
+    } catch (...) {
+//        Using catch(...) to handle every type of Exceptions.
+        cout << "Something Wrong...";
+    }
 }
