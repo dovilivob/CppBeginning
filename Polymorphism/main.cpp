46f176c9923de653953f4dc6d2261f7f24076934
PC <david.iii.chiu@gmail.com>
2022-03-03 14:56:23 +0800

    polymorphism done

--- Polymorphism/main.cpp
+++ Polymorphism/main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include <list>
+#include <cmath>
+
+using namespace std;
+
+class Animal {
+public:
+    void animalSound() {
+        cout << "Fuck! Fuck!! Fuck!!!" << endl;
+    }
+};
+
+class Pig : public Animal {
+public:
+    void animalSound() {
+        cout << "I'm a pig, kiss me." << endl;
+    }
+};
+
+class Dog : public Animal {
+public:
+    void animalSound() {
+        cout << "I wanna eat pork!!" << endl;
+    }
+
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Animal animal;
+    Pig pig;
+    Dog dog;
+    animal.animalSound();
+    pig.animalSound();
+    dog.animalSound();
     return 0;
 }
