b5f538e81be1ba4fe4a5a58d75e8519d6b6ff4cd
PC <david.iii.chiu@gmail.com>
2022-03-03 15:59:34 +0800

    fstream done

--- fstream/main.cpp
+++ fstream/main.cpp
@@ -1,6 +1,25 @@
+//  ofstream: Creates and writes to files.
+//  ifstream: Read from files.
+//  fstream: creates, reads, and writes to files.
+
 #include <iostream>
+#include <fstream>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+//    Create ./cmake-build-debug/Testing/file.txt
+    ofstream myFile("file.txt");
+    myFile << "Something";
+    myFile.close();
+
+//    Read a file
+    string txt;
+    ifstream myReadFile("file.txt");
+//    Using while loop with getline() to read line by line.
+    while(getline(myReadFile, txt)){
+        cout << txt;
+    }
+    myReadFile.close();
     return 0;
 }
