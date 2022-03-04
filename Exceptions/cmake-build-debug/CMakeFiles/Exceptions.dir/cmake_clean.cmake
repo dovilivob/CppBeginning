3ce479bd93d54982b8159071ddb193fb5daeb2ba
dovilivob <david.iii.chiu@gmail.com>
2022-03-04 10:51:00 +0800

    ddd

---
+++ Exceptions/cmake-build-debug/CMakeFiles/Exceptions.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Exceptions.dir/main.cpp.obj"
+  "CMakeFiles/Exceptions.dir/main.cpp.obj.d"
+  "Exceptions.exe"
+  "Exceptions.exe.manifest"
+  "Exceptions.pdb"
+  "libExceptions.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Exceptions.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
