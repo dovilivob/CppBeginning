758bddb9c50fbc69940cd204d7332d8ba8db97b4
PC <david.iii.chiu@gmail.com>
2022-03-06 14:56:39 +0800

    tripleX Built

---
+++ TripleX/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+void playGameAtDifficulty(int difficulty){
+
+}
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
